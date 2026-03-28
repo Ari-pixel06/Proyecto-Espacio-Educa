28d124f513cb31043451245c53ebef20fae50082
Ari-pixel06 <suarezarianny06@gmail.com>
2026-03-28 11:32:49 -0400

    Correciones Vercel

--- src/screens/AddCard.tsx
+++ src/screens/AddCard.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useCards, type CardData } from '../contexts/CardsContext';
 import './VistaMazo.css';
